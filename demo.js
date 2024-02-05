785de30a259a355fc634ab6835ababe28904aa74
dmitryimalkov <dmitry.i.malkov@gmail.com>
2024-02-05 16:17:51 +0300

    all

--- demo.js
+++ demo.js
@@ -1,10 +1,17 @@
-let inputElement = document.querySelector("input");
-let rest = 60;
+let productNameInputElement = document.getElementById('product-name');
+let remainingCharsElement = document.getElementById('remaining-chars');
 
-function retriveUserInput() {
-//  let enteredText = inputElement.value;
-  rest = rest - 1;
-  return rest;
+// console.dir(productNameInputElement);
+
+let maxAllowedChars = productNameInputElement.maxLength;
+
+function updateRemainingCharacters(event) {
+  let enteredText = event.target.value;
+  let enteredTextLength = enteredText.length; 
+
+  let remainingCharacters = maxAllowedChars - enteredTextLength;
+
+  remainingCharsElement.textContent = remainingCharacters;
 }
 
-inputElement.addEventListener("input", retriveUserInput);
+productNameInputElement.addEventListener('input', updateRemainingCharacters);
